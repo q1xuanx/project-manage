b7e186afb60b45b8c84187d774bc0bc34ae62987
nhan <nhoang2929@gmail.com>
2024-12-17 18:44:03 +0700

    begin

---
+++ src/test/java/projects/manage/ManageApplicationTests.java
@@ -0,0 +1,13 @@
+package projects.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
